b68f083ac8d7457226d0bd1b274505819d47b936
Santiago Lopez <santiago.lopez@ekumenlabs.com>
2024-12-17 10:50:16 -0300

    Convert SpectacularAI dataset to NeRF Capture format.

--- bash_scripts/nerfcapture.bash
+++ bash_scripts/nerfcapture.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # check rmem_max and wmem_max, and increase size if necessary
-if [ "$#" -ne 1 ]; then
-    echo "Usage: bash_scripts/nerfcapture.bash <config_file>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: bash_scripts/nerfcapture.bash <config_file> <input_dataset>"
     exit
 fi
 
@@ -25,6 +25,23 @@ else
     sudo sysctl -w net.core.wmem_max=2147483647
 fi
 
+nerfcapture_dir=$(dirname $2)/nerfcapture
+# Convert Spectacular AI dataset to NeRF Capture.
+python3 spectacularAI-sdk/python/mapping/replay_to_nerf.py $2 \
+    --format=nerfcapture --fast \
+    --image_format=png \
+    --device_preset=ios-tof \
+    --key_frame_distance=0.0001 \
+    $nerfcapture_dir
+
+FRAMES=$(ls -l $nerfcapture_dir/rgb | wc -l)
+if [ -z FRAMES ] || [ FRAMES -eq 0 ]; then
+    echo "Not enough frames at $nerfcapture_dir/rgb!"
+    exit
+fi
+
+# 
+python3 scripts/nerfcapture2dataset.py --config $1 --dataset $nerfcapture_dir --frames $FRAMES
 # Capture Dataset
 python3 scripts/nerfcapture2dataset.py --config $1
 
